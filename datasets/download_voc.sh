85bad0cc254f49b7d3e65f531776471a4b0e4027
root <long.vo210404@vnuk.edu.vn>
2024-03-15 15:48:20 +0000

    Send code

---
+++ datasets/download_voc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR=~/fcn-pytorch-implement/datasets/VOC
+mkdir -p $DIR
+cd $DIR
+
+if [ ! -e benchmark_RELEASE ]; then
+    wget https://www2.eecs.berkeley.edu/Research/Projects/CS/vision/grouping/semantic_contours/benchmark.tgz
+    tar -xvf benchmark.tgz
+fi
+
+if [ ! -e VOCdevkit/VOC2012 ]; then
+    wget http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar
+    tar -xvf VOCtrainval_11-May-2012.tar
+fi
