18815e5650472c709ec19dae72be55ea37b7343b
root <long.vo210404@vnuk.edu.vn>
2024-03-16 05:38:42 +0000

    Send codes

--- datasets/.ipynb_checkpoints/download_voc-checkpoint.sh
+++ datasets/.ipynb_checkpoints/download_voc-checkpoint.sh
@@ -4,12 +4,12 @@ DIR=~/fcn-pytorch-implement/datasets/VOC
 mkdir -p $DIR
 cd $DIR
 
-if [ ! -e benchmark_RELEASE ]; then
-    wget https://www2.eecs.berkeley.edu/Research/Projects/CS/vision/grouping/semantic_contours/benchmark.tgz
-    tar -xvf benchmark.tgz
-fi
-
-# if [ ! -e VOCdevkit/VOC2012 ]; then
-#     wget http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar
-#     tar -xvf VOCtrainval_11-May-2012.tar
+# if [ ! -e benchmark_RELEASE ]; then
+#     wget https://www2.eecs.berkeley.edu/Research/Projects/CS/vision/grouping/semantic_contours/benchmark.tgz
+#     tar -xvf benchmark.tgz
 # fi
+
+if [ ! -e VOCdevkit/VOC2012 ]; then
+    wget http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar
+    tar -xvf VOCtrainval_11-May-2012.tar
+fi
